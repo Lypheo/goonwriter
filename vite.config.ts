1b382ce3556c10e52398337a55ac7f9dc4482271
Lypheo <saifullah124@hotmail.de>
2026-02-13 02:50:33 +0100

    add proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Expose to local network
     watch: {
       // Enable polling for WSL2 on Windows drives
       usePolling: true,
       interval: 1000,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
     },
   },
 })
